96b8840d30822b77889874c6850f5a16f5ded0eb
Rachael Alexanderson <eruanna@drdteam.org>
2017-02-19 19:49:19 -0500

    - Updated by dpJudas, gets ProductVersion instead now.

--- GetProductVersion.cpp
+++ GetProductVersion.cpp
@@ -3,8 +3,16 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 #pragma comment(lib, "version.lib")
+
+struct LANGANDCODEPAGE
+{
+	WORD wLanguage;
+	WORD wCodePage;
+};
 
 int main(int argc, char **argv)
 {
@@ -21,6 +29,31 @@ int main(int argc, char **argv)
 		return 254;
 	}
 
+	LANGANDCODEPAGE *lpTranslate;
+	UINT cbTranslate;
+	if (!VerQueryValueA(data, "\\VarFileInfo\\Translation", (void**)&lpTranslate, &cbTranslate))
+	{
+		std::cout << "Could not get file version info from " << argv[1] << std::endl;
+		return 253;
+	}
+
+	for (size_t i = 0; i < (cbTranslate / sizeof(struct LANGANDCODEPAGE)); i++)
+	{
+		char bull[1024];
+		sprintf_s(bull, "\\StringFileInfo\\%04x%04x\\ProductVersion", lpTranslate[i].wLanguage, lpTranslate[i].wCodePage);
+
+		char *str;
+		UINT strlen;
+		if (!VerQueryValueA(data, bull, (void**)&str, &strlen))
+		{
+			std::cout << "Could not get file version info from " << argv[1] << std::endl;
+			return 253;
+		}
+		std::cout << str << std::endl;
+		break;
+	}
+
+	/*
 	VS_FIXEDFILEINFO *fileinfo;
 	UINT fileinfolength;
 	if (!VerQueryValueA(data, "\\", (void**)&fileinfo, &fileinfolength))
@@ -28,8 +61,8 @@ int main(int argc, char **argv)
 		std::cout << "Could not get file version info from " << argv[1] << std::endl;
 		return 253;
 	}
-
 	std::cout << (fileinfo->dwProductVersionMS >> 16) << "." << (fileinfo->dwProductVersionMS & 0xffff) << std::endl;
+	*/
 
     return 0;
 }
